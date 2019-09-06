6c6990a9a32f9c8ab7d4f08bac8eedb4c0215ce2
Gary Gambill <mcsegary@yahoo.com>
2019-09-06 15:38:17 -0500

    fixed vue demo for ios

--- demo-vue/app/components/App.vue
+++ demo-vue/app/components/App.vue
@@ -8,20 +8,23 @@
         <Label class="message" :text="`longitude: ${location.longitude}`" />
         <Label class="message" :text="`accuracy: ${location.accuracy}`" />
       </StackLayout>
-      <Label class="message" :text="`errorLog: ${errorLog}`" col="0" row="2"/>
-      <Label class="message" :text="`events: ${events}`" col="0" row="3"/>
-      <Label class="message" :text="`trackonce: ${trackonce}`" col="0" row="4"/>
-      <!-- <StackLayout col="0" row="5">
-        <Label class="message" :text="`currentLocation latitude: ${currentLocation.latitude}`" />
-        <Label class="message" :text="`currentLocation longitude: ${currentLocation.longitude}`" />
-        <Label class="message" :text="`currentLocation accuracy: ${currentLocation.accuracy}`" />
-      </StackLayout> -->
+
+      <Label v-for="(error, index) in errorLog" :key="index" class="message" :text="`error: ${error}`" col="0" row="2"/>
+      <Label v-for="(event, index) in events" :key="index" class="message" :text="`event: ${event}`" col="0" row="3"/>
+      <Label class="message" :text="`trackonce status: ${trackonce}`" col="0" row="4"/>
+      <StackLayout col="0" row="5" v-if="computedLocation.location">
+        <Label class="message" :text="`currentLocation latitude: ${computedLocation.location.latitude}`" />
+        <Label class="message" :text="`currentLocation longitude: ${computedLocation.location.longitude}`" />
+        <Label class="message" :text="`currentLocation accuracy: ${computedLocation.location.accuracy}`" /> 
+      </StackLayout>
     </GridLayout>
   </Page>
 </template>
 
 <script lang="ts">
   import { RadarIO } from 'nativescript-radar-io';
+  import * as geolocation from "nativescript-geolocation";
+  import { Accuracy } from "tns-core-modules/ui/enums";
 
   export default {
     data() {
@@ -31,63 +34,76 @@
         errorLog: '',
         events: '',
         trackonce: '',
-        currentLocation: '',
+        currentLocation: {}
       }
     },
     async created() {
       await RadarIO.requestPermissions(true);
+
       RadarIO.startTracking({
         offline: 'replayOff',
         sync: 'all',
         priority: 'responsiveness'
       });
+
       if (!RadarIO.isTracking()) {
         this.isTracking = false;
       } else {
         this.isTracking = true;
       }
+
       RadarIO.on('location', (event) => {
-          console.log('location');
-          console.dir({
-              location: event.object.get('location'),
-              user: event.object.get('user')
-          });
-          this.location = event.object.get('location');
+        console.log('location');
+        console.dir({
+          location: event.object.get('location'),
+          user: event.object.get('user')
+        });
+        this.location = event.object.get('location');
       });
+
       RadarIO.on('error', error => {
-          console.log('error');
-          console.dir({
-              error: error.object.get('error')
-          });
-          this.errorLog = error.object.get('error');
+        console.log('error');
+        console.dir({
+          error: error.object.get('error')
+        });
+        this.errorLog = error.object.get('error');
       });
+
       RadarIO.on('events', events => {
-          console.log('events');
-          console.dir({
-              events: events.object.get('events'),
-              user: events.object.get('user')
-          })
-          this.events = events.object.get('events');
+        console.log('events');
+        console.dir({
+          events: events.object.get('events'),
+          user: events.object.get('user')
+        })
+        this.events = events.object.get('events');
       });
+      
       RadarIO.trackOnce().then(result => {
-          console.log('trackonce');
-          console.dir(result);
-          this.trackonce = result;
+        console.log('trackonce');
+        console.dir(result);
+        this.trackonce = result.status;
       });
-      // this.getCurrentLocation();
+
+      this.updateCurrentLocation();
     },
     methods: {
-      // getCurrentLocation({timeout: 10000}).then(location => {
-      //   return RadarIO.updateLocation({
-      //       accuracy: location.horizontalAccuracy,
-      //       longitude: location.longitude,
-      //       latitude: location.latitude
-      //   })
-      // }).then(result => {
-      //     console.log('updateLocation');
-      //     console.dir(result);
-      //     this.currentLocation = result;
-      // })
+      updateCurrentLocation() {
+        return geolocation.getCurrentLocation({ desiredAccuracy: Accuracy.high, maximumAge: 5000, timeout: 5000 }).then(location => {
+          return RadarIO.updateLocation({
+            accuracy: location.horizontalAccuracy,
+            longitude: location.longitude,
+            latitude: location.latitude
+          })
+        }).then(result => {
+            this.currentLocation = result;
+        })
+      }
+    },
+    computed: {
+      computedLocation() {
+        console.log('computedLocation - ', this.currentLocation)
+        return this.currentLocation;
+      }
     }
   }
 </script>
@@ -97,7 +113,7 @@
         background-color: #53ba82;
         color: #ffffff;
     }
-ß
+
     .message {
         vertical-align: center;
         text-align: left;
--- demo-vue/app/main.ts
+++ demo-vue/app/main.ts
@@ -1,4 +1,7 @@
 import Vue from 'nativescript-vue'
+import * as app from 'tns-core-modules/application';
+import { RadarIO } from 'nativescript-radar-io';
+import { isIOS } from 'tns-core-modules/platform';
 import App from './components/App.vue'
 
 import VueDevtools from 'nativescript-vue-devtools'
@@ -10,16 +13,14 @@ if(TNS_ENV !== 'production') {
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = true; //(TNS_ENV === 'production')
 
-import { RadarIO } from 'nativescript-radar-io';
-import { isIOS } from 'tns-core-modules/platform';
-
-RadarIO.initialize("prj_test_pk_b38b9b97c5ea101516d494c9c688afb32db27ba2");
-RadarIO.setUserId('demo-vue');
-RadarIO.setDescription('Vue Demo ' + (isIOS ? 'iOS': 'Android') );
-RadarIO.setMetadata({
-    name: 'Test User 1'
+app.on(app.launchEvent, async (args) => {
+  RadarIO.initialize("prj_test_pk_b38b9b97c5ea101516d494c9c688afb32db27ba2");
+  RadarIO.setUserId("demo-vue");
+  RadarIO.setDescription("Vue Demo " + (isIOS ? "iOS" : "Android"));
+  RadarIO.setMetadata({
+    name: "Test User 1"
+  });
 });
-
 
 new Vue({
   render: h => h('frame', [h(App)])
