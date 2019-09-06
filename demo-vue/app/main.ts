b7af02c86902b1bfcd3bf6eb7e32029642bacf3f
Gary Gambill <mcsegary@yahoo.com>
2019-09-06 11:52:53 -0500

    minor fixes

--- demo-vue/app/main.ts
+++ demo-vue/app/main.ts
@@ -10,18 +10,15 @@ if(TNS_ENV !== 'production') {
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = true; //(TNS_ENV === 'production')
 
-// import { RadarIO } from 'nativescript-radar-io';
 import { RadarIO } from 'nativescript-radar-io';
 import { isIOS } from 'tns-core-modules/platform';
 
-RadarIO.initialize("prj_test_pk_98f103a8def0642a9ac4168b58ed26b50eeb1fc7");  // gary
-// RadarIO.initialize("prj_test_pk_40052cf0a885c689dd45df97d1f76eb22f5b80f5");  // carissa
-RadarIO.setUserId('demo-vue-garyg-tester2');
+RadarIO.initialize("prj_test_pk_b38b9b97c5ea101516d494c9c688afb32db27ba2");
+RadarIO.setUserId('demo-vue');
 RadarIO.setDescription('Vue Demo ' + (isIOS ? 'iOS': 'Android') );
 RadarIO.setMetadata({
-    name: 'Gary Gambill'
+    name: 'Test User 1'
 });
-
 
 
 new Vue({
